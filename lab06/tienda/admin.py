abbbe4e30343d651f2f4cd0e3783c3aac02f87e8
DiegoEscobedo17 <90659176+DiegoEscobedo17@users.noreply.github.com>
2022-05-03 09:49:19 -0500

    lab08

--- lab06/tienda/admin.py
+++
@@ -1,9 +0,0 @@
-import imp
-from django.contrib import admin
-
-# Register your models here.
-from .models import Categoria
-from .models import Producto
-
-admin.site.register(Categoria)
-admin.site.register(Producto)
